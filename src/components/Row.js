a127b1a32f4bb95f09ebf11a0048dd4ca9655e8f
FNogueira2023 <f.nogueira2022@outlook.com>
2022-12-13 12:18:48 -0300

    Agregado proptypes

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,9 @@
 import React from "react";
 import '../assets/css/table.css';
+import PropTypes from 'prop-types';
 
 function Row(props) {
-    return(
+    return (
         <React.Fragment>
             <tr>
                 <td>{props.cell1}</td>
@@ -14,5 +15,32 @@ function Row(props) {
         </React.Fragment>
     )
 }
+
+Row.defaultProps = {
+    cell1: "Sin información",
+    cell2: "Sin información",
+    cell3: "Sin información",
+    cell4: "Sin información",
+    cell5: "Sin información",
+
+}
+
+
+Row.propTypes = {
+    cell1: PropTypes.string,
+    cell2: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    cell3: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    cell4: PropTypes.string,
+    cell5: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ])
+};
 
 export default Row;
